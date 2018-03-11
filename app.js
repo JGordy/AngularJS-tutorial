7b42d41df42c811205a972f6798485df19023c03
Joseph Gordy <jgordy2424@gmail.com>
2018-03-10 22:43:42 -0500

    Added ajax call to external API and displayed the data on the page using ng-repeat

--- app.js
+++ app.js
@@ -1,22 +1,29 @@
 (function() {
   var app = angular.module('store', []);
 
-  app.controller('StoreController', function() {
-    this.products = games;
 
-  });
-
-  var games = [
-    {
-      name: "Scrabble",
-      type: "Board",
-      description: "Super nerdy word game that everyone loves"
-    },
-    {
-      name: "Exploding Kittens",
-      type: "Card",
-      description: "Survive the Exploding Kitten onslaught!"
+  app.factory("fetchGameList", function($http) {
+    this.getlist = function() {
+        return $http.get('https://house-rules-jgwrbs.herokuapp.com/api/gameList', {'Access-Control-Allow-Origin': 'localhost:*'})
+            .then(function(response) {
+              // console.log(response); //I get the correct items, all seems ok here
+              return response.data;
+            });
     }
-  ];
+    return this;
+  }); // end of the fetchGameList factory
 
-})();
+
+
+
+  app.controller('StoreController', [ '$scope', 'fetchGameList', function( $scope, fetchGameList ) {
+    $scope.games = [];
+    // this.games = games;
+    fetchGameList.getlist().then(function(arrItems) {
+      $scope.games = arrItems;
+      console.log($scope.games);
+    });
+  }]); // end of the StoreController
+
+
+})(); //end of app module
